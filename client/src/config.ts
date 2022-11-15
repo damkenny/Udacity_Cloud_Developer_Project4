370dac9f9ddd1369d06c72abb8e28c95cb6b870d
damkenny <adeniyekehinde0@gmail.com>
2022-11-15 15:52:37 +0100

    new branch

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'cliwtfybsd'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-a6xpbmcl5cz5dpjb.us.auth0.com',            // Auth0 domain
+  clientId: 'Mo1q7tkLyuO5UyuYKHUMHGOKdqPKPMwM',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
